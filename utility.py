8258cf0b2ddcb2415d2d7a0b83a13ac52b2b9620
ccoop4 <charlescooper821@gmail.com>
2024-09-13 13:33:43 -0500

    added bidreictional

--- utility.py
+++ utility.py
@@ -1,12 +1,15 @@
 import networkx as nx
 
 def kmax_diameter(graph):
-    if nx.is_connected(graph):
+    if nx.is_strongly_connected(graph):
         return nx.diameter(graph)
     else:
         sum = 0
-        S = [graph.subgraph(c).copy() for c in nx.connected_components(graph)]
+        S = [graph.subgraph(c).copy() for c in nx.weakly_connected_components(graph)]
         for sub in S:
             sum += nx.diameter(sub)
         print("not sure what to put here yet so I just summed the diameter of the connected components")
         return sum
+    
+def count_bidrectional(graph):
+    return 0.5 * len([ 1 for (u,v) in graph.edges() if u in graph[v] ])
