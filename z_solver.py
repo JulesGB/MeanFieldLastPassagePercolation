22841058c0a4f4fe17c18de90e2126967b66f602
Jules Goduco-Bunting <Jules.Goduco-Bunting@sandc.com>
2025-02-20 17:16:56 -0600

    z_solver changes

--- z_solver.py
+++ z_solver.py
@@ -11,7 +11,8 @@ def transform(G, H, lam, delta):
     G1 = G1 - G1[-1] + H1[-1]
     return G1, H1
 
-def compute_z(lam, num_samples, num_iterations=20, print_iters=False):
+# N = num_samples
+def compute_z_pdf(lam, num_samples, num_iterations=20, print_iters=False):
     delta = 0.5 / num_samples # delta = 0.5/N for numerical integration
     H = np.zeros(num_samples)
     G = -1 * np.cumsum(A(H)) * delta/lam
@@ -21,8 +22,18 @@ def compute_z(lam, num_samples, num_iterations=20, print_iters=False):
         G, H = transform(G, H, lam, delta)
         if print_iters:
             print('Iteration = %2d, Mass at 0 = %10.9f' % (i+1, 1-lam * G[0]))
+
+    X = np.linspace(0, .5, num_samples)
+    x = np.concatenate((X, 0.5+X[1:]), axis=None)
+    y = 1 - lam * np.concatenate((G[:-1], H[::-1]), axis=None)
+    xs = x[1:]
+    ys = np.c_[y[1:], np.diff(y)/delta]
     
-    return G, H
+    #plt.figure()
+    #plt.plot(xs, ys, '-', label=['CDF', 'density'])
+    #plt.legend()
+    
+    return xs, ys
 
 """
 X = np.linspace(0, .5, N)
